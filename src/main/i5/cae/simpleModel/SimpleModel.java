7cb05eddc178ce736cb979b9d56f29d8162ee855
PedeLa <lange@dbis.rwth-aachen.de>
2015-07-08 13:57:37 +0200

    removed not needed values

--- src/main/i5/cae/simpleModel/SimpleModel.java
+++ src/main/i5/cae/simpleModel/SimpleModel.java
@@ -10,8 +10,8 @@ import i5.cae.simpleModel.node.SimpleNode;
  * 
  * Simplified representation of a SyncMeta model. Lacks style information (position elements), JSON
  * converting functionality and persistence options. Also aggregates the Model-Attributes group into
- * the model itself. Used as a read-only element send via the LAS2peer network to the
- * CAE-Code-Generation Service.
+ * the model itself and removes data only used by database operations (ids..). Used as a read-only
+ * element send via the LAS2peer network to the CAE-Code-Generation Service.
  * 
  * Part of the CAE-Simple-Model-Representation.
  *
@@ -20,7 +20,6 @@ public class SimpleModel implements Serializable {
 
   private static final long serialVersionUID = -7841290138342593951L;
 
-  private int id;
   private String name;
   private ArrayList<SimpleNode> nodes;
   private ArrayList<SimpleEdge> edges;
@@ -30,24 +29,18 @@ public class SimpleModel implements Serializable {
    * 
    * Creates a new model.
    * 
-   * @param modelId the id of this @link{SimpleModel}
    * @param name the name of this @link{SimpleModel}
    * @param nodes an @link{ArrayList} of @link{SimpleNode}s
    * @param edges an @link{ArrayList} of @link{SimpleEdge}s
    * @param attributes an @link{ArrayList} of @link{SimpleEntityAttribute}s
    * 
    */
-  public SimpleModel(int modelId, String name, ArrayList<SimpleNode> nodes,
-      ArrayList<SimpleEdge> edges, ArrayList<SimpleEntityAttribute> attributes) {
-    this.id = modelId;
+  public SimpleModel(String name, ArrayList<SimpleNode> nodes, ArrayList<SimpleEdge> edges,
+      ArrayList<SimpleEntityAttribute> attributes) {
     this.name = name;
     this.nodes = nodes;
     this.edges = edges;
     this.attributes = attributes;
-  }
-
-  public int getId() {
-    return id;
   }
 
   public String getName() {
